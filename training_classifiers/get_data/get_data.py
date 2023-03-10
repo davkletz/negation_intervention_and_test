c1264fe83684018ceebbaa6719148a4c335eb16d
davkletz <david.kletz@sorbonne-nouvelle.fr>
2023-03-10 13:15:49 +0100

    Update get_data.py

--- training_classifiers/get_data/get_data.py
+++ training_classifiers/get_data/get_data.py
@@ -6,6 +6,25 @@ path  = "/data/mnedeljkovic/thesis/thesis/code/embeddings"
 
 
 pages = 10000
-embedds = torch.load(f"{path}/embeddings{pages}")
+embedds : dict = torch.load(f"{path}/embeddings{pages}")
 
 print(type(embedds))
+print(embedds.keys())
+
+
+dico_detail = {}
+
+tot_p = 0
+tot_n = 0
+
+for key in embedds.keys():
+    nb_p = len(embedds[key][1])
+    nb_n = len(embedds[key][0])
+
+    dico_detail[key] = (nb_p, nb_n)
+    tot_n += nb_n
+    tot_p += nb_p
+
+print(dico_detail)
+
+print(tot_p, tot_n)

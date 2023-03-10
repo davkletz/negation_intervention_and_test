7ca4445f16ee5edab2a32772821960115caf12e2
davkletz <david.kletz@sorbonne-nouvelle.fr>
2023-03-10 13:04:44 +0100

    Update get_data.py

--- training_classifiers/get_data/get_data.py
+++ training_classifiers/get_data/get_data.py
@@ -6,6 +6,6 @@ path  = "/data/mnedeljkovic/thesis/thesis/code/embeddings"
 
 
 pages = 10000
-embedds = torch.load(f"{path}/embeddings_{pages}")
+embedds = torch.load(f"{path}/embeddings{pages}")
 
 print(type(embedds))

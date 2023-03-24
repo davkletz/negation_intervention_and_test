1e07b386fc32ae996bf9fd900c4092d81177a9e4
davkletz <david.kletz@sorbonne-nouvelle.fr>
2023-03-24 11:44:56 +0100

    Update proj_spaces.py

--- training_classifiers/rlace/tests/proj_spaces.py
+++ training_classifiers/rlace/tests/proj_spaces.py
@@ -20,7 +20,9 @@ def get_projection(P, rank):
 
 
 
-path = "/home/dkletz/tmp/pycharm_project_99/2022-23/Interventions/with_neg_eval/negation_intervention_and_test/training_classifiers/rlace"
+#path = "/home/dkletz/tmp/pycharm_project_99/2022-23/Interventions/with_neg_eval/negation_intervention_and_test/training_classifiers/rlace"
+
+path = "/data/dkletz/Experiences/negation_intervention_and_test/training_classifiers/rlace"
 
 with open(f"{path}/Proj_50000.pkl", "rb") as f:
     P = pkl.load(f)

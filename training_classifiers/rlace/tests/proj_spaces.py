d051077e6cc7921cb41d315f78b80055e7f6a0e5
davkletz <david.kletz@sorbonne-nouvelle.fr>
2023-03-24 15:44:07 +0100

    Update proj_spaces.py

--- training_classifiers/rlace/tests/proj_spaces.py
+++ training_classifiers/rlace/tests/proj_spaces.py
@@ -2,7 +2,7 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 import pickle as pkl
-
+import copy
 import numpy as np
 from sklearn.linear_model import SGDClassifier
 import torch.nn as nn
@@ -96,11 +96,11 @@ device = torch.device("cuda:0") if torch.cuda.is_available() else "cpu"
 
 net = RobertaLMHead2().to(device)
 
-criterion = nn.CrossEntropyLoss()
+criterion = nn.BCELoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 X_torch, y_torch = torch.from_numpy(X).to(device), torch.from_numpy(y).to(device)
-
+X_torch, y_torch = X_torch[:20], y_torch[:20]
 
 for epoch in range(1):  # loop over the dataset multiple times
 
@@ -130,16 +130,16 @@ print('Finished Training')
 
 correct = 0
 total = 0
+best_acc = 0
 # since we're not training, we don't need to calculate the gradients for our outputs
 with torch.no_grad():
-    for i, data in enumerate(X_torch):
-        images, labels = X_torch[i], y_torch[i]
-        # calculate outputs by running images through the network
-        outputs = net(images)
-        # the class with the highest energy is what we choose as prediction
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+    net.eval()
+    y_pred = net(X_torch)
+    acc = (y_pred.round() == y_torch).float().mean()
+    acc = float(acc)
+    if acc > best_acc:
+        best_acc = acc
+        best_weights = copy.deepcopy(net.state_dict())
 
 print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
 
